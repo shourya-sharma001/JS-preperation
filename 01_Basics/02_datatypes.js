75d5493c41901a7383f863843a35942c18261b29
Shourya Sharma <shourya4971@gmail.com>
2025-01-05 13:19:08 +0000

    Basics

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //This will throw an error, as we are using node js not browser, the browser has its own compiler engine while in the case of the node js this is not supported.
+
+ let name = "SHOURYA";
+ let age = 20;
+ let isMarried = false;
+
+ // number => 2 to power 53;
+ // bigInt
+ // string => ""
+ // boolean => true/false
+ // symbol => unique and immutable
+ // null and undefined => primitive types
+
+
+ // object => reference type
+ // let person = {
+ //     name: "SHOURYA",
+ //     age: 20,
+ //     isMarried: false
+ // }
+
+
+ // typeof ____ => Tells the datatype of a variable.
+
+
+ console.log(typeof "Shourya");//String
+ console.log(typeof null);// object
+ console.log(typeof undefined);//Undefined
+ console.log(typeof 3);//number
